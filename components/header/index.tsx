1ef3bc42f931a1669af6401b6046a7986ec31bbc
Hafizu <piz230601@gmail.com>
2024-12-04 13:22:20 +0800

    change text

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -69,7 +69,7 @@ export default function Home() {
 						<div className="flex flex-col justify-between h-full p-10">
 							<div className="flex flex-col space-y-5">
 								<div className="text-3xl pb-5 text-gray-800 uppercase border-b text-center border-gray-600 mb-4">
-									PLTW X UTHM
+									GAN 356 I3
 								</div>
 								{navItems.map((item, index) => (
 									<Link
@@ -81,7 +81,7 @@ export default function Home() {
 										<ChevronLeft className="w-8 h-8 ms-3 transition-transform transform group-hover:-translate-x-2 group-hover:animate-in" />
 									</Link>
 								))}
-								</div>
+							</div>
 						</div>
 					</motion.div>
 				</SheetContent>
